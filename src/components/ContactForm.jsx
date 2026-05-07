64a3f7c7492a24a3a2aef2bcfb591dd27dd66ad8
Jason Dagana <j.dagana@teflonhub.com>
2026-05-08 00:31:27 +0100

    formatted ContactForm component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
-}
+  return <div>ContactForm</div>;
+};
 
-export default ContactForm
+export default ContactForm;
