9f16893b81c5febc6d608a7d9bb2780c39008889
Jason Dagana <j.dagana@teflonhub.com>
2026-05-16 19:19:30 +0100

    designed the how it work section how planetary data helps section

--- src/components/HowPlanetaryDataHelps.jsx
+++ src/components/HowPlanetaryDataHelps.jsx
@@ -1,7 +1,24 @@
+import VideoPlayer from "./VideoPlayer"
 
 const HowPlanetaryDataHelps = () => {
   return (
-    <div>HowPlanetaryDataHelps</div>
+     <section className="how-it-works">
+          <div className="video-demo">
+            <VideoPlayer />
+            <div className="text-wrapper">
+              <h2>How Planetary Data Helps Us Understand Space</h2>
+              <p>
+                Planetary science goes beyond images. Comparing{" "}
+                <span className="muted-text">mass</span>,{" "}
+                <span className="muted-text">diameter</span>,{" "}
+                <span className="muted-text">gravity</span>, and{" "}
+                <span className="muted-text">density</span>, we gain insight
+                into how planets form, behave, and interact within the solar
+                system.
+              </p>
+            </div>
+          </div>
+        </section>
   )
 }
 
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,20 +1,23 @@
-
 const VideoPlayer = () => {
   return (
-    <video
-  autoplay
-  muted
-  loop
-  webkit-playsinline
-  class="absolute inset-0 w-full h-full object-cover"
->
-  <source
-    src="https://youtu.be/z8aBZZnv6y8?si=2x-SzGdzXQitpFmb"
-    type="video/mp4"
-  />
-  Your browser does not support the video tag.
-</video>
-  )
-}
+    <div className="video-container">
+      <video
+        autoplay
+        muted
+        loop
+        webkit-playsinline
+        controls
+        class="absolute inset-0 w-full h-full object-cover"
+      >
+        <source
+          src="https://res.cloudinary.com/jasond/video/upload/v1769963776/4389357-uhd_3840_2024_30fps_lyenee.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
 
-export default VideoPlayer
+
+export default VideoPlayer;
