4ba43e84a6a3f3d46a60541ecda65d0f5ad7512e
Bethel Amadi <amadibethel@icloud.com>
2026-05-20 03:44:11 +0200

    Add Button file to TS Academy Group 15 Capstone Project

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,43 +1,4 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva } from "class-variance-authority"
-
-
-const buttonVariants = cva(
-  "shared-features",
-  {
-    variants: {
-      variant: {
-        default:
-          "primary-button",
-          outline:
-            "outline-button",
-        destructive:
-          "",
-        secondary:
-          "",
-        ghost: "",
-        link: "",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
-
-const Button = React.forwardRef(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={buttonVariants({ variant, className })}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = "Button"
-
-export { Button, buttonVariants }
+<div className="cta-btns">
+                  <Button variant="default"> Explore the Data</Button>
+                  <Button variant="outline"> Contact Us</Button>
+                </div>
